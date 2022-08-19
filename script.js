a7fbbb36567225851beb1cb448da792ca0cb3a21
FadirTorcer <eric.lyonford@gmail.com>
2022-08-18 19:44:37 -0500

    First real shot at this assignment

--- script.js
+++ script.js
@@ -2,6 +2,28 @@
 
 window.addEventListener("load", function() {
 
+    let form = document.querySelector("form");
+    form.addEventListener("submit", function(event) {
+        /* pilotName" id="pilotName"/></label>
+                </div>
+                <div class="formField">
+                    <label>Co-pilot Name <input type="text" name="copilotName"/></label>
+                </div>
+                <div class="formField">
+                    <label>Fuel Level (L) <input type="text" name="fuelLevel"/></label> 
+                </div>
+                <div class="formField">
+                    <label>Cargo Mass (kg) <input type="text" name="cargoMass */
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      
+      if (usernameInput.value === "" || teamName.value === "") {
+        alert("All fields are required!");
+        // stop the form submission
+        event.preventDefault();
+      }
+    });
+
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
    let listedPlanetsResponse;
--- scriptHelper.js
+++ scriptHelper.js
@@ -19,15 +19,40 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 function validateInput(testInput) {
     if (testInput === "") {
         return "Empty";
-    } else if (typeof(testInput) === "string") {
-        return "String";
-    } else if (typeof(testInput) === "number") {
-        return "Not a string";
+    } else if (isNan(testInput)) {
+        return "Not a Number";
+    } else if (isInteger(testInput)) {
+        return "Is a Number";
     }
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+   /*
+   notReady = document.getElementById(faultyItems);
+   gasTank = document.getElementById(fuelStatus);
+   manifest = document.getElementById(cargoStatus);
+   firstPilot = document.getElementById(pilotStatus);
+   secondPilot = document.getElementById(coPilotStatus);
+   launchReadiness = document.getElementById(launchStatus);
+
+   firstPilot.innerHTML = `Pilot ${pilot} Ready`;
+   secondPilot.innerHTML = `Co-pilot ${copilot} Ready`;
+
+   if (fuelLevel <= 10000) {
+    notReady.style.visibility = "visible";
+    gasTank.innerHTML = "Fuel level IS NOT high enough for launch";
+    launchReadiness.innerHTML = "Shuttle not ready for launch";
+    launchReadiness.style.color = "red";
+   } else if (cargoLevel >= 10000) {
+    notReady.style.visibility = "visible";
+    manifest.innerHTML = "Cargo level IS TOO HIGH for launch";
+    launchReadiness.innerHTML = "Shuttle not ready for launch";
+    launchReadiness.style.color = "red";
+   } else {
+    notReady.style.visibility = "visible";
+    launchReadiness.innerHTML = "Shuttle is ready for launch";
+   }
+   */
 }
 
 async function myFetch() {
